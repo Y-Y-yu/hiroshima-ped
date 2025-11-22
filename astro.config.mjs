8da71b472ea665ed7c5bc1ebd2d91f0b21006855
yuki <yuki.shitashita@gmail.com>
2025-11-22 23:30:29 +0900

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import preact from '@astrojs/preact';
-
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://blog.hiroshima-ped.com/',
   output: 'server',
   adapter: vercel(),
-  integrations: [preact(), partytown()],
+  integrations: [preact(), partytown(), sitemap()],
 });
